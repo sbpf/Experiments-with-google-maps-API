5d7237e9f84aca01b19b4c32fdf65e64d327b46c
Sindhu Shankar <sindhu.pathfinder@gmail.com>
2018-04-03 15:22:26 -0700

    initial commit neighborhood map

---
+++ js/apiCalls.js
@@ -0,0 +1,82 @@
+/*-----------------------------------------------------------------------------------
+//GOOGLE MAPS API--------------------------------------------------------------------
+//---------------------------------------------------------------------------------*/
+function initMap(){    
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.3879876, lng: -122.0913282},
+        zoom: 13
+    });
+
+    //Create an empty single info window, which can hold data
+    //when any of the markers is clicked.
+    infoWindow = new google.maps.InfoWindow({
+        content: ""
+    });  
+}
+
+//This function creates the marker
+function createMarker(markerData){ 
+    markerPosition = {lat: markerData.lat , lng: markerData.lng};
+    var marker = new google.maps.Marker({
+        position: markerPosition,
+        map: map,
+        title: markerData.name
+    });      
+
+    //Add a listener event to markers for animating when clicked          
+    marker.addListener('click',function(){
+        setCurrentLocation(markerData.name);
+        animateMarker(marker);
+        populateInfoWindow(markerData.name,marker);
+    });  
+    return marker; 
+}
+
+//Display relevant markers based on the selected menu option -Ex.Restaurants
+function displayRelevantListings(typeOfListing){
+    infoWindow.close();
+   
+    locationList.forEach(function(markerItem){        
+       if(typeOfListing == markerItem.locationType || typeOfListing =='All'){        
+            markerItem.marker.setVisible(true);
+
+            //If the location is not available in the listing, push it
+            var index = self.listings.indexOf(markerItem.name);
+            if(index == -1){
+                self.listings.push(markerItem.name);
+            }                                     
+       }
+       else{
+            markerItem.marker.setVisible(false);
+
+            //This location should not be displayed as it doesn't belong to the selected listing category
+            //Hence remove it from the listings
+            var index = self.listings.indexOf(markerItem.name);           
+            if (index > -1) {
+                self.listings.splice(index, 1);
+            }
+       }
+    });
+}
+
+function highlightMarker(selectedLocation){
+    var index = locationList.findIndex(x => x.name==selectedLocation);
+    var selectedMarker = locationList[index];
+    animateMarker(selectedMarker.marker);
+    populateInfoWindow(selectedMarker.name,selectedMarker.marker)
+}
+
+function animateMarker(marker){
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function () { marker.setAnimation(null);}, 1400);    
+}
+
+function populateInfoWindow(name,marker){
+    infoWindow.setContent(name);
+    infoWindow.open(map,marker);
+}
+
+function setCurrentLocation(name){
+    var index = locationList.findIndex(x => x.name==selectedLocation);
+    self.currentSelectedLocation(locationList[index]);
+}
